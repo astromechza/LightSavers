aef4cd86d290d84d62629d8a9bde5d401c683bab
Siobhan O'Donovan <siobhan.odonovan@hotmail.com>
2013-08-13 13:15:14 +0200

    Started to design Player class - Set up Bones

---
+++ Projects/LightSavers/LightSavers/LightSavers/Components/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//imports taken from Tank class
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace LightSavers.Components
+{
+    class Player
+    {
+        //***INITIAILISNG ATTRIBUTES OF PLAYER OBJECT***//
+
+        //MODEL AS A WHOLE
+        Model playerModel;
+
+        //MODEL BONES
+        //here we need to figure out exactly which bones we have. 
+        //fingers??
+        ModelBone neck;
+
+        //left size
+        ModelBone leftShoulder;
+        ModelBone leftElbow;
+        ModelBone leftWrist;
+
+        ModelBone leftHip;
+        ModelBone leftKnee;
+        ModelBone leftAnkle;
+
+        //right size
+        ModelBone rightShoulder;
+        ModelBone rightElbow;
+        ModelBone rightWrist;
+
+        ModelBone rightHip;
+        ModelBone rightKnee;
+        ModelBone rightAnkle;
+    }
+}
