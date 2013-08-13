aaeea915f58aba17e301de9fa28108f85a056a8f
Siobhan O'Donovan <siobhan.odonovan@hotmail.com>
2013-08-13 16:09:24 +0200

    Added Load method. Starting with Draw. Still fleshing out Player.

--- Projects/LightSavers/LightSavers/LightSavers/Components/Player.cs
+++ Projects/LightSavers/LightSavers/LightSavers/Components/Player.cs
@@ -21,24 +21,115 @@ namespace LightSavers.Components
         //MODEL BONES
         //here we need to figure out exactly which bones we have. 
         //fingers??
-        ModelBone neck;
+        ModelBone neckBone;
 
         //left size
-        ModelBone leftShoulder;
-        ModelBone leftElbow;
-        ModelBone leftWrist;
+        ModelBone leftShoulderBone;
+        ModelBone leftElbowBone;
+        ModelBone leftWristBone;
 
-        ModelBone leftHip;
-        ModelBone leftKnee;
-        ModelBone leftAnkle;
+        ModelBone leftHipBone;
+        ModelBone leftKneeBone;
+        ModelBone leftAnkleBone;
 
         //right size
-        ModelBone rightShoulder;
-        ModelBone rightElbow;
-        ModelBone rightWrist;
+        ModelBone rightShoulderBone;
+        ModelBone rightElbowBone;
+        ModelBone rightWristBone;
 
-        ModelBone rightHip;
-        ModelBone rightKnee;
-        ModelBone rightAnkle;
+        ModelBone rightHipBone;
+        ModelBone rightKneeBone;
+        ModelBone rightAnkleBone;
+
+        //MATRICES TO STORE ORIGINAL TRANSFORMS
+        Matrix neckTransform;
+
+        //left size
+        Matrix leftShoulderTransform;
+        Matrix leftElbowTransform;
+        Matrix leftWristTransform;
+
+        Matrix leftHipTransform;
+        Matrix leftKneeTransform;
+        Matrix leftAnkleTransform;
+
+        //right size
+        Matrix rightShoulderTransform;
+        Matrix rightElbowTransform;
+        Matrix rightWristTransform;
+
+        Matrix rightHipTransform;
+        Matrix rightKneeTransform;
+        Matrix rightAnkleTransform;
+
+        //ARRAY OF TRANSFORMATIONS TO STORE ALL TRANSFORMATIONS
+        Matrix[] boneTransforms;
+
+        //DIRECTIONS AND POSITIONS
+        public Vector3 Position;
+        public Vector3 Direction;
+        public Vector3 Up;
+        public Vector3 right;
+
+
+        //LOAD METHOD
+        public void Load(ContentManager content)
+        {
+            //loads correct model for player
+            playerModel = content.Load<Model>("");
+
+            //load all the bones
+            neckBone = playerModel.Bones[""];
+
+            leftShoulderBone = playerModel.Bones[""];
+            leftElbowBone = playerModel.Bones[""];
+            leftWristBone = playerModel.Bones[""];
+            leftHipBone = playerModel.Bones[""];
+            leftKneeBone = playerModel.Bones[""];
+            leftAnkleBone = playerModel.Bones[""];
+
+            rightShoulderBone = playerModel.Bones[""];
+            rightElbowBone = playerModel.Bones[""];
+            rightWristBone = playerModel.Bones[""];
+            rightHipBone = playerModel.Bones[""];
+            rightKneeBone = playerModel.Bones[""];
+            rightAnkleBone = playerModel.Bones[""];
+
+            //load the transforms
+            neckTransform = neckBone.Transform;
+
+            leftShoulderTransform = leftShoulderBone.Transform;
+            leftElbowTransform = leftElbowBone.Transform;
+            leftWristTransform = leftWristBone.Transform;
+            leftHipTransform = leftHipBone.Transform;
+            leftKneeTransform = leftKneeBone.Transform;
+            leftAnkleTransform = leftAnkleBone.Transform;
+
+            rightShoulderTransform = rightShoulderBone.Transform;
+            rightElbowTransform = rightElbowBone.Transform;
+            rightWristTransform = rightWristBone.Transform;
+            rightHipTransform = rightHipBone.Transform;
+            rightKneeTransform = rightKneeBone.Transform;
+            rightAnkleTransform = rightAnkleBone.Transform;
+
+            //initialisng the transform matrix array
+            boneTransforms = new Matrix[playerModel.Bones.Count];
+
+            //setting directions
+            Direction = new Vector3(1, 0, 0);
+            Up = Vector3.Up;
+        }
+
+        //GETTING USER INPUT TO MOVE THE PLAYER AROUND
+        public void getUserInput(GamePadState currentGamePadState, KeyboardState currentKeyboardState, GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+
+        }
+
     }
 }
