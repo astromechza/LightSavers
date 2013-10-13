5d62c32039afbd498448bfe6682c3498f6b8f3e3
Ben Meier <benmeier42@gmail.com>
2013-10-13 17:55:45 +0200

    Added primitive limited stack structures

---
+++ Projects/LightSavers/LightSavers/LightSavers/Utils/IntStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LightSavers.Utils
+{
+    public class IntStack
+    {
+        int[] queue;
+        int cursor;
+
+        public IntStack(int size)
+        {
+            queue = new int[size];
+            cursor = 0;
+        }
+
+        public void Reset()
+        {
+            cursor = 0;
+        }
+
+        public void Push(int v)
+        {
+            queue[cursor] = v;
+            cursor++;
+        }
+
+        public int Pop()
+        {
+            return queue[--cursor];
+        }
+
+        public bool IsEmpty()
+        {
+            return cursor == 0;
+        }
+
+    }
+}
